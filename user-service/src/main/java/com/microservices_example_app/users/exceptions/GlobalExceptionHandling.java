a147a6342104d7f52feb1f2a5af6e01c73a02304
KillRmnv <memes20006@gmail.com>
2026-04-25 14:45:22 +0300

    fix(user-service): -rename entry point -add missing annotations

--- user-service/src/main/java/com/microservices_example_app/users/exceptions/GlobalExceptionHandling.java
+++ user-service/src/main/java/com/microservices_example_app/users/exceptions/GlobalExceptionHandling.java
@@ -17,12 +17,19 @@ public class GlobalExceptionHandling {
                 body(Map.of("message:",ex.getMessage(),
                         "status",HttpStatus.NOT_FOUND));
     }
-
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String,Object>> illegalArgumentException(IllegalArgumentException ex){
         return ResponseEntity.
                 status(HttpStatus.BAD_REQUEST).
                 body(Map.of("message:",ex.getMessage(),
                         "status",HttpStatus.BAD_REQUEST));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String,Object>> unexpectedException(Exception ex){
+        return ResponseEntity.
+                status(HttpStatus.INTERNAL_SERVER_ERROR).
+                body(Map.of("message: Unexpected exception:",ex.getMessage(),
+                        "status",HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
 }
--- user-service/src/test/java/com/microservices_example_app/users/TestWebApplication.java
+++ user-service/src/test/java/com/microservices_example_app/users/TestWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(WebApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(UserServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
