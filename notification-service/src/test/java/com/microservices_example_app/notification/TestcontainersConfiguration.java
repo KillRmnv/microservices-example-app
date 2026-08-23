98ea62016e3f5bf126f28d8642bf22198c91b74a
KillRmnv <memes20006@gmail.com>
2026-08-23 22:38:41 +0300

    test: add testcontainer sprind-boot dependency

--- notification-service/src/test/java/com/microservices_example_app/notification/TestcontainersConfiguration.java
+++ notification-service/src/test/java/com/microservices_example_app/notification/TestcontainersConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.kafka.KafkaContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -17,11 +16,6 @@ class TestcontainersConfiguration {
 		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
 	}
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-	}
 
 	@Bean
 	@ServiceConnection(name = "redis")
