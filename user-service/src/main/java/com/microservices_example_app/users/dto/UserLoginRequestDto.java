b00acc105e81c5f87a824c2d5c3b7634d0818ffe
KillRmnv <memes20006@gmail.com>
2026-08-23 23:30:13 +0300

    fix (user service): add checks to the user login

--- user-service/src/main/java/com/microservices_example_app/users/dto/UserLoginRequestDto.java
+++ user-service/src/main/java/com/microservices_example_app/users/dto/UserLoginRequestDto.java
@@ -1,12 +1,16 @@
 package com.microservices_example_app.users.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-
 public class UserLoginRequestDto {
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be valid")
     private String email;
+    @NotBlank(message = "Password must not be blank")
     private String password;
 }
