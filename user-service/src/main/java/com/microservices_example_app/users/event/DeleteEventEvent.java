502a8390e1788982ba02672e7f7473b2ce4f5622
KillRmnv <memes20006@gmail.com>
2026-08-23 23:21:00 +0300

    fix(user service): fix wrong annotation

--- user-service/src/main/java/com/microservices_example_app/users/event/DeleteEventEvent.java
+++ user-service/src/main/java/com/microservices_example_app/users/event/DeleteEventEvent.java
@@ -1,6 +1,7 @@
 package com.microservices_example_app.users.event;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 public class DeleteEventEvent {
-    @NotBlank(message = "Event must not be blank")
+    @NotEmpty(message = "Events must not be empty")
     private List<String> events;
     @NotBlank(message = "Source service must not be blank")
     private String sourceService;
